d85277f5ca0a6affadd10466e069d768fa533b46
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2024-03-19 02:57:59 +0100

    moving section view all jobs from App to a separate ViewAllJobs component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import NavBar from "./components/NavBar";
 import Hero from "./components/Hero";
 import HomeCard from "./components/HomeCard";
 import JobListings from "./components/JobListings";
+import ViewAllJobs from "./components/ViewAllJobs";
 
 const App = () => {
   return (
@@ -9,17 +10,8 @@ const App = () => {
       <NavBar />
       <Hero />
       <HomeCard />
-
       <JobListings />
-      
-      <section className="m-auto max-w-lg my-10 px-6">
-        <a
-          href="jobs.html"
-          className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
-        >
-          View All Jobs
-        </a>
-      </section>
+      <ViewAllJobs />
     </>
   );
 };
