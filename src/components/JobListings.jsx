7932ecfee7b9a3c81f4c9d7a7d62564c4f102e21
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2024-03-19 17:16:12 +0100

    making proxy to json server i.e. vite config

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -8,7 +8,7 @@ const JobListings = ({ isHome = false }) => {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    const apiUrl = isHome ? 'http://localhost:8000/jobs?_limit=3' : 'http://localhost:8000/jobs'
+    const apiUrl = isHome ? "/api/jobs?_limit=3" : "/api/jobs";
     // definition
     const fetchJobs = async () => {
       try {
@@ -32,15 +32,15 @@ const JobListings = ({ isHome = false }) => {
         <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
           {isHome ? "Recent Jobs" : "Browse Jobs"}
         </h2>
-          {loading ? (
-            <Spinner loading={loading} />
-          ) : (
-            <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-              {jobs.map((job) => (
-                <JobListing key={job.id} job={job} />
-              ))}
-            </div>
-          )}
+        {loading ? (
+          <Spinner loading={loading} />
+        ) : (
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+            {jobs.map((job) => (
+              <JobListing key={job.id} job={job} />
+            ))}
+          </div>
+        )}
       </div>
     </section>
   );
